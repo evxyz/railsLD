ce31b6c6d3118b0860574910235c133253f0cfa8
evx001 <eviexeris@yahoo.com>
2012-05-28 16:21:31 -0400

    prebundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsLD::Application.config.secret_token = '4023715d1c49a3efc7568fc0cf04fa2d504635d2334fa49123ac1017af50ec0a321c064dec67ac0585ac7ff072dc38f5a2aa199f7e62eb4308043c33e3c5f79a'
